0b26e26c189aea9639a49a419a13ed15ca84d517
razan <razanmazen8@gmail.com>
2023-01-28 13:51:47 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let logInBtn = document.getElementByClassName('logbtn');
+let logInDiv = document.getElementById('logIn');
+logInBtn.onClick = function(){
+    logInDiv.classList.toggle('show');
+
+}
+
+
