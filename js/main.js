230eec75ab16992b8c345b1ad23ddcd88f5fb103
razan <razanmazen8@gmail.com>
2023-01-31 13:05:01 +0200

    new

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 let logInBtn = document.getElementByClassName('logbtn');
 let logInDiv = document.getElementById('logIn');
+let orderlist = document.getElementByClassName('list-order');
+let orderBtn = document.querySelector('.order button a');
 logInBtn.onClick = function(){
     logInDiv.classList.toggle('show');
+
+}
+orderBtn.onClick= function(){
+    orderlist.style.display= 'block';
 
 }
 
